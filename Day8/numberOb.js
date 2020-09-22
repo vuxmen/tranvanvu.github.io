8fc4e2efd52c48fa991a03e5a152fd7a57d9afbe
Tran Van Vu <vuxmen@iMac-cua-Tran.local>
2020-09-22 07:43:14 +0700

    continue JS numb+ob

--- Day8/numberOb.js
+++ Day8/numberOb.js
@@ -2,11 +2,14 @@ var radius = '';
 var int1 = null;
 var int2 = null;
 var number = null;
+var d = ''
+var int = null;
+
 $('.btn-check').eq(0).click(function() {
 	radius = $('input').eq(0).val();
 	console.log(typeof radius);
 	if (radius < 0 || radius === undefined || isNaN(radius) === true) {
-		$('.spn-check').eq(0).html(' Nhập sai dữ liệu');
+		$('.spn-check').eq(0).html(' Nhập sai');
 	} else if (radius.length === 0) {
 		$('.spn-check').eq(0).html(' Chưa nhập dữ liệu');
 	}
@@ -17,7 +20,7 @@ $('.btn-check').eq(0).click(function() {
 
 $('.btn-result').eq(0).click(function() {
 	if (radius < 0 || radius === undefined || isNaN(radius) === true) {
-		$('.spn-result').eq(0).html('Nhập sai dữ liệu');
+		$('.spn-result').eq(0).html('Nhập sai');
 	} else if (radius.length === 0) {
 		$('.spn-result').eq(0).html('Chưa nhập dữ liệu');
 	} 
@@ -30,7 +33,7 @@ $('.btn-check').eq(1).click(function() {
 	var a = $('input').eq(1).val();
 	var b = $('input').eq(2).val();
 	if (a >= 0 || b >= 0 || isNaN(a) === true|| isNaN(b) === true) {
-		$('.spn-check').eq(1).html('Nhập sai dữ liệu');
+		$('.spn-check').eq(1).html('Nhập sai');
 	} else {
 		$('.spn-check').eq(1).html('Ok');
 		int1 = parseInt(a);
@@ -42,7 +45,7 @@ $('.btn-check').eq(1).click(function() {
 $('.btn-result').eq(1).click(function() {
 	var sum = 0;
 	if (int1 >= 0 || int2 >= 0 || isNaN(int1) === true|| isNaN(int2) === true) {
-		$('.spn-result').eq(1).html('Nhập sai dữ liệu');
+		$('.spn-result').eq(1).html('Nhập sai');
 	} else {
 		if (int1 <= int2) {
 			sum = (int1 + int2) * (int2 - int1 + 1) / 2 - int1 - int2;
@@ -55,18 +58,86 @@ $('.btn-result').eq(1).click(function() {
 
 $('.btn-check').eq(2).click(function() {
 	var c = $('input').eq(3).val();
-	console.log(isNaN(number));
-	if (isNaN(number) === true) {
-		console.log(number);
-		$('.spn-check').eq(2).html('Nhập sai dữ liệu');
+	console.log(isNaN(c));
+	if (isNaN(c) === true) {
+		console.log(c);
+		$('.spn-check').eq(2).html('Nhập sai');
 	} else {
 		$('.spn-check').eq(2).html('Ok');
-		var number = parseInt(c);
+		number = parseInt(c);
 	}
 });
+
 $('.btn-result').eq(2).click(function() {
-	
+	if (number < 2) {
+		$('.spn-result').eq(2).html('Không là số nguyên tố');
+	} else {
+		for (var i = 2; i < number; i ++) {
+			if (number % i === 0) {
+				$('.spn-result').eq(2).html('Không là số nguyên tố');	
+				return;
+			} else {
+				$('.spn-result').eq(2).html('Là số nguyên tố');
+			}
+		}
+	} 
 });
+
+$('.btn-check').eq(3).click(function() {
+	d = $('input').eq(4).val();
+	if (d === '') {
+		$('.spn-check').eq(3).html('Chưa nhập số');
+	}
+	else if (isNaN(d) === true || d <= 2) {
+		$('.spn-check').eq(3).html('Nhập sai, giá trị nhập vào là số nguyên >=3');
+	} else {
+		$('.spn-check').eq(3).html('Ok');
+		int = parseInt(d);
+	}
+ });
+
+$('.btn-result').eq(3).click(function() {
+	var tong = 0;
+	if (d === '') {
+		$('.spn-result').eq(3).html('Chưa nhập số');
+	} else if (isNaN(d) === true || d <= 2) {
+		$('.spn-result').eq(3).html('Nhập sai');
+	} 
+	for (var i = 2; i < int; i ++) {
+		if (int % i !== 0) {
+			tong = tong + i ;
+		}
+	}
+	$('spn-result').eq(3).html('Tổng các số nguyên tố <= ' + int + ' là: ' + tong);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
