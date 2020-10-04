48a057eff6fcad69bf1ebb0e3fdb1386d3a31a02
Macbook <macbook@Macbooks-iMac.local>
2020-10-05 06:14:36 +0700

    Final Exam Completed

--- Final Exam/finalExam.js
+++ Final Exam/finalExam.js
@@ -1,84 +1,137 @@
 
-var container = document.querySelector('#css-container');
-var carousel = document.querySelector('#css-carousel');
-var carouselChild = document.querySelectorAll('.carousel-child');
-var preButton = document.querySelector('#css-preButton');
-var nextButton = document.querySelector('#css-nextButton');
-var counter = 1;
-var width = container.clientWidth;
-var distance = 0;
+let container = document.querySelector('#css-container');
+let carousel = document.querySelector('#css-carousel');
+let carouselChild = document.querySelectorAll('.carousel-child');
+let preButton = document.querySelector('#css-preButton');
+let nextButton = document.querySelector('#css-nextButton');
+let counter = 1;
+let width = container.clientWidth;
 
-carousel.style.transform = 'translateX(' + (-width) + 'px)';
+let jscontainer = document.querySelector('#js-container');
+let jscarousel = document.querySelector('#js-carousel');
+let jscarouselChild = document.querySelectorAll('.js-carousel-child');
+let jspreButton = document.querySelector('#js-preButton');
+let jsnextButton = document.querySelector('#js-nextButton');
+let jscounter = 1;
+let jswidth = jscontainer.clientWidth;
 
+carousel.style.transform = 'translateX(' + (-counter * width) + 'px)';
+jscarousel.style.transform = 'translateX(' + (-jscounter * jswidth) + 'px)';
+
+//Automatic carousel of HTML, CSS part every 4s
+setInterval(function() {
+	if (counter >= carouselChild.length - 1 ) return;
+	carousel.style.transition = 'transform 0.4s linear';
+	counter ++;
+	carousel.style.transform = 'translateX(' + (-counter * width) + 'px)';
+}, 4000);
+
+//Automatic carousel of JS part every 4s
+setInterval(function() {
+	if (jscounter >= jscarouselChild.length - 1 ) return;
+	jscarousel.style.transition = 'transform 0.4s linear';
+	jscounter ++;
+	jscarousel.style.transform = 'translateX(' + (-jscounter * width) + 'px)';
+}, 3000);
+
+//Function for click in HTML, CSS part
 preButton.addEventListener('click', () => {
 	if (counter <= 0) return;
 	carousel.style.transition = 'transform 0.4s linear';
 	counter --;
-	distance = counter * width;
-	carousel.style.transform = 'translateX(' + (-distance) + 'px)'; 
+	carousel.style.transform = 'translateX(' + (-counter * width) + 'px)'; 
 
 });
 
 nextButton.addEventListener('click', () => {
-	if (counter >= carouselChild.length -1 ) return;
+	if (counter >= carouselChild.length - 1 ) return;
 	carousel.style.transition = 'transform 0.4s linear';
 	counter ++;
-	distance = counter * width;
-	carousel.style.transform = 'translateX(' + (-distance) + 'px)'; 
+	carousel.style.transform = 'translateX(' + (-counter * width) + 'px)'; 
 });
 
+//Function for transitionend in HTML, CSS part
 carousel.addEventListener('transitionend', () => {
-	if (carouselChild[counter].id === 'first') {
+	if (carouselChild[counter].id === 'css-first') {
+		console.log('counter = ' + counter);
 		carousel.style.transition = 'none';
-		carousel.style.transform = 'translateX(' + (-width) + 'px)';
-		counter = 1;
+		counter = carouselChild.length - counter;
+		carousel.style.transform = 'translateX(' + (-counter * width) + 'px)';
 	}
-   	if (carouselChild[counter].id === 'last') {
+   	if (carouselChild[counter].id === 'css-last') {
+   		console.log('counter = ' + counter);
       	carousel.style.transition = 'none';
-      	carousel.style.transform = 'translateX(' + -(carouselChild.length - 2) * width + 'px)';
-      	counter = 5;
+      	counter = carouselChild.length - 2;
+      	carousel.style.transform = 'translateX(' + (-counter * width)+ 'px)';
     }
 })
 
-
-
-var jscontainer = document.querySelector('#js-container');
-var jscarousel = document.querySelector('#js-carousel');
-var jscarouselChild = document.querySelectorAll('.js-carousel-child');
-var jspreButton = document.querySelector('#js-preButton');
-var jsnextButton = document.querySelector('#js-nextButton');
-var jscounter = 1;
-var jswidth = jscontainer.clientWidth;
-var jsdistance = 0;
-
-jscarousel.style.transform = 'translateX(' + (-jswidth) + 'px)';
-
+//Function for click in JS part
 jspreButton.addEventListener('click', () => {
 	if (jscounter <= 0) return;
 	jscarousel.style.transition = 'transform 0.4s linear';
 	jscounter --;
-	jsdistance = jscounter * jswidth;
-	jscarousel.style.transform = 'translateX(' + (-jsdistance) + 'px)'; 
+	jscarousel.style.transform = 'translateX(' + (-jscounter * jswidth) + 'px)'; 
 
 });
 
 jsnextButton.addEventListener('click', () => {
-	if (jscounter >= jscarouselChild.length -1 ) return;
+	if (jscounter >= jscarouselChild.length - 1 ) return;
 	jscarousel.style.transition = 'transform 0.4s linear';
 	jscounter ++;
-	jsdistance = jscounter * jswidth;
-	jscarousel.style.transform = 'translateX(' + (-jsdistance) + 'px)'; 
+	jscarousel.style.transform = 'translateX(' + (-jscounter * jswidth) + 'px)'; 
 });
 
+//Function for transitionend in JS part
 jscarousel.addEventListener('transitionend', () => {
-	if (jscarouselChild[jscounter].id === 'first') {
+	if (jscarouselChild[jscounter].id === 'js-first') {
 		jscarousel.style.transition = 'none';
-		jscarousel.style.transform = 'translateX(' + (-jswidth) + 'px)';
-		jscounter = 1;
+		jscounter = jscarouselChild.length - jscounter;
+		jscarousel.style.transform = 'translateX(' + (-jscounter * jswidth) + 'px)'; 
 	}
-   	if (jscarouselChild[jscounter].id === 'last') {
+   	if (jscarouselChild[jscounter].id === 'js-last') {
       	jscarousel.style.transition = 'none';
-      	jscarousel.style.transform = 'translateX(' + -(jscarouselChild.length - 2) * jswidth + 'px)';
-      	jscounter = 5;
+      	jscounter = jscarouselChild.length - 2;
+      	jscarousel.style.transform = 'translateX(' + (-jscounter * jswidth) + 'px)'; 
     }
 })
+
+//Introduction CODE
+// let aboutMe = {
+// 	name: 'Tran Van Vu',
+// 	born: 1991,
+// 	city: 'Ha Noi',
+// 	interested in: ['Code', 'Game', 'Music', 'Book', 'Film', 'Sport']
+// }
+// var lifeForCode = true;
+// var CodeForlife = true;
+// var retire = 2040;
+
+// function Introduction(_aboutMe, _lifeForCode, _CodeForlife,_retire) {
+// 	while (_lifeForCode || _CodeForlife) {
+// 		console.log('CodeForlife or lifeForCode');
+// 		_aboutMe.born ++;
+// 		if (_aboutMe.born > retire) break;
+// 	}
+// }
+
+// Introduction(aboutMe, lifeForCode, CodeForlife, retire);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
