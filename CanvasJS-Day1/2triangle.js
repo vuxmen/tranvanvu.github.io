b242ecd7376c825f3759e4c8d34a5e68b97e7277
vuxmen_git <vutranvan.libra@gmail.com>
2020-10-23 16:23:11 +0700

    draw 2 triangle

---
+++ CanvasJS-Day1/2triangle.js
@@ -0,0 +1,23 @@
+let canvas = document.getElementById('canvas');
+let pen = canvas.getContext('2d');
+canvas.width = 1200;
+canvas.height = 400;
+canvas.style.border = '1px solid red';
+canvas.style.margin = 'auto';
+pen.strokeStyle = 'black';
+pen.fillStyle = 'black';
+pen.moveTo(100, 100);
+pen.lineTo(200, 100);
+pen.lineTo(100, 200);
+pen.lineTo(100, 100);
+pen.fill();
+pen.beginPath();
+pen.moveTo(100, 220);
+pen.lineTo(200, 120);
+pen.lineTo(200, 220);
+pen.lineTo(100, 220);
+pen.stroke();
+
+
+
+
