858cf1307d25ffd2a4b9cb61863e52c5f8d4a85b
vuxmen_git <vutranvan.libra@gmail.com>
2020-10-25 13:56:09 +0700

    finished beta PointGame

--- CanvasJS-Day4/pointgame.js
+++ CanvasJS-Day4/pointgame.js
@@ -14,13 +14,15 @@ let username = 'vuxmen';
 let audio = new Audio('http://dight310.byu.edu/media/audio/FreeLoops.com/5/5/Mouse%20Click%20Fast.wav-23232-Free-Loops.com.mp3');
 let numb = 0;
 let size = 12;
+let state = true;
 
 class Ball {
-	constructor(x, y, radius, color) {
+	constructor(x, y, radius, color, fontSize) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
 		this.color = color;
+		this.fontSize = fontSize;
 		this.dx = 0;
 		this.dy = 0;
 	}
@@ -33,24 +35,32 @@ class Ball {
 		pen.fill();
 	}
 
-	drawBoss() {
-		if (numb % 10 == 0 && numb != 0 && numb != arrball.length) {
-			this.radius *= 1.005;
-			size *=1.005;
-		}
-
+	drawBoss(arrball) {
 		pen.shadowBlur = 20;
 		pen.shadowColor = "green";
+		if (numb >= 1/3 * arrball.length) {
+			this.radius = 33;
+			this.fontSize = 14;
+			this.color = 'green';
+		}
+		if (numb >= 2/3 * arrball.length) {
+			this.radius = 35;
+			this.fontSize = 15.5;
+			this.color = 'blue';
+		}
+		if (numb == arrball.length) {
+			this.radius = 37;
+			this.fontSize = 16;
+			this.color = 'red';
+		}
 		pen.beginPath();
-		pen.fillStyle = this.color;
 		pen.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		pen.fillStyle = this.color;
 		pen.fill();
-
 		pen.beginPath();
-		pen.font = `${size}px Georgia`;
+		pen.font = `${this.fontSize}px Georgia`;
 		pen.fillStyle = 'white';
 		pen.fillText(username, this.x - 1.4 * this.radius/2, this.y + 0.1 * this.radius);
-
 	}
 
 	move(arrball) {
@@ -73,9 +83,9 @@ class Ball {
 	}
 }
 
-let bossBall = new Ball(60, 60, 30, 'orange');
+let bossBall = new Ball(60, 60, 30, 'orange', 12);
 let arrball = [];
-for (let i =0; i < 30; i ++) {
+for (let i = 0; i < 30; i ++) {
 	let radius = 8;
 	let x = Math.floor(Math.random() * (canvas.width - radius));
 	let y = Math.floor(Math.random() * (canvas.height - radius));
@@ -104,33 +114,42 @@ document.addEventListener('keydown', () => {
 	}
 });
 
-function animate() {
-	if (numb == 30) {
-		setTimeout(function(){
-			
-		}, 2000);
-		return
+function winGameAlert() {
+	if (numb == arrball.length) {
+		setTimeout(() => {
+			window.alert('YOU WON THE GAME!!!');
+			state = false;
+		}, 1000);
 	}
-
-	requestAnimationFrame(animate);
-	console.log('abc');
-
-	pen.clearRect(0, 0, canvas.width, canvas.height);
-
-	bossBall.drawBoss();
-
-	pen.beginPath();
-	pen.fillStyle = 'white';
-	pen.font = '50px Georgia';
-	pen.fillText(numb, 100, canvas.height - 50);
-	
-	arrball.forEach(element => {
-		element.draw();
-	});
-	bossBall.move(arrball);
 }
 
-animate();
+
+function animate() {
+	winGameAlert();
+	if (state == false) {
+		window.cancelAnimationFrame(animate);
+		return
+	} else if (state == true) {
+		window.requestAnimationFrame(animate);
+		console.log('abc');
+
+		pen.clearRect(0, 0, canvas.width, canvas.height);
+
+		bossBall.drawBoss(arrball);
+
+		pen.beginPath();
+		pen.fillStyle = 'white';
+		pen.font = '50px Georgia';
+		pen.fillText(numb, 100, canvas.height - 50);
+		
+		arrball.forEach(element => {
+			element.draw();
+		});
+		bossBall.move(arrball);
+	}
+}
+
+animate(winGameAlert);
 
 
 
