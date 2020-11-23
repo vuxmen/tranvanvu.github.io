1b88b987946d5e66e54c835b693c7adc7ba60662
vuxmen <vutranvan.libra@gmail.com>
2020-11-23 12:51:08 +0700

    memory card

--- memory-cards/main.js
+++ memory-cards/main.js
@@ -3,104 +3,66 @@ let clear = document.querySelector('#clear');
 let pre = document.querySelector('#prev');
 let next = document.querySelector('#next');
 let container = document.querySelector('#cards-container');
-let card = container.querySelectorAll('.card');
 let table = document.querySelector('#add-container');
-let current = 0;
-let arrcard = [];
 let question = document.querySelector('#question');
 let answer = document.querySelector('#answer');
 let addcard = document.querySelector('#add-card');
 let front = document.querySelector('.inner-card-front');
 let back = document.querySelector('.inner-card-back');
 let showanswer = false;
+let current = 0;
+let card;
+let cardData = [
+	{
+		question: 'How different DOMstring vs String ?',
+		answer: 'A DOMString is a sequence of 16-bit unsigned integers. This corresponds exactly to the JavaScript primitive String type.'
+	},
+	{
+		question: 'What is the same method name of array and string type in Javascript ?',
+		answer: '.length'
+	}
+];
 
-card.forEach(e => {
-	arrcard.push(e);
-});
+setup();
 
-console.log(arrcard);
-arrcard[0].classList.add('active');
-
-show.addEventListener('click', () => {
-	table.classList.add('show');
-});
-
-addcard.addEventListener('click', () => {
-	let newcard = document.createElement('div');
-	let innerCard = document.createElement('div');
-	let frontCard = document.createElement('div');
-	let backCard = document.createElement('div');
-	let para1 = document.createElement('p');
-	let para2 = document.createElement('p');
-	innerCard.classList.add('inner-card');
-	frontCard.classList.add('inner-card-front');
-	backCard.classList.add('inner-card-back');
-	frontCard.appendChild(para1);
-	backCard.appendChild(para2);
-	innerCard.appendChild(frontCard);
-	innerCard.appendChild(backCard);
-	newcard.appendChild(innerCard);
-	newcard.classList.add('class');
-	
-	container.innerHTML += `<div class="card">
+function setup() {
+	container.innerHTML = '';
+	cardData.forEach(
+		e => {
+				container.innerHTML += `<div class="card">
 				<div class="inner-card">
-					<div class="inner-card-front">
+						<div class="inner-card-front">
 						<p>
-							${question.value}
-						</p>
+						${e.question}
+					</p>
 					</div>
 					<div class="inner-card-back">
 						<p>
-							${answer.value}
-						</p>
+							${e.answer}
+ 						</p>
+	 					</div>
 					</div>
-				</div>
-			</div>`;
+				</div>`
+				}
+		);
+	card = document.querySelectorAll('.card');
+	card[current].classList.add('active');
+}
 
-	table.classList.remove('show');
-	arrcard.push(newcard);
-	console.log(arrcard);
-
-});
-
-clear.addEventListener('click', () => {
-	arrcard = [];
-	container.innerHTML = '';
-
+pre.addEventListener('click', () => {
+	console.log('pre');
+	current ++;
+	if (current > card.length) return;
+	card[current-1].classList.add('left');
+	card[current-1].classList.remove('active', 'right');
+	card[current].classList.add('active');
 });
 
 next.addEventListener('click', () => {
-	if (current == arrcard.length - 1) return
-	arrcard[current].classList.remove('active');
-	arrcard[current].classList.add('left');
-	current ++;
-	arrcard[current].classList.remove('right');
-	arrcard[current].classList.add('active');
-	
-});
-
-pre.addEventListener('click', () => {
-	
-	if (current == 0) return
-	arrcard[current].classList.remove('active');
-	arrcard[current].classList.add('right');
+	console.log('next');
 	current --;
-	
-	arrcard[current].classList.add('active');
-	arrcard[current].classList.remove('left');
-
+	if (current < 0) return;
+	card[current+1].classList.add('right');
+	card[current+1].classList.remove('active', 'left');
+	card[current].classList.add('active');
 });
-
-card.forEach(e => e.addEventListener('click', () => {
-	switch(showanswer) {
-		case false:
-		e.classList.add('show-answer');
-		showanswer = true;
-		break;
-		case true: 
-		e.classList.remove('show-answer');
-		showanswer = false;
-		break;
-	}
-	
-}));
