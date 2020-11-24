3b72b7c30d4fcbce346c7fcb21e127cee0f41ff9
Macbook <macbook@Macbooks-iMac.local>
2020-11-25 00:11:51 +0700

    processing Quiz

--- Quiz/main.js
+++ Quiz/main.js
@@ -4,9 +4,24 @@ let finish = document.querySelector('.finish-btn');
 let quickProgress = document.querySelector('.quiz-progress-bar');
 let quizContent = document.querySelector('.quiz-content');
 let quizQuestion = document.querySelector('.quiz-question');
-let quizAnswer = document.querySelector('.quiz-answer');
+let quizAnswer;
+let quizItem;
+let input;
 let current = 0;
-let dataStore = [];
+let dataStore = [
+    {
+      current: 0,
+      index: null
+    },
+    {
+      current: 1,
+      index: null
+    },
+    {
+      current: 2,
+      index: null
+    },
+];
 
 let data = [
     {
@@ -52,19 +67,21 @@ function reset() {
         next.classList.remove('disable');
         pre.classList.remove('disable');
     }
-   
+
 }
 
 initialize();
 
 function initialize() {
-    quizContent.innerHTML += 
+    quizContent.innerHTML +=
         `<div class="quiz-question">
             <p>${data[current].question}</p>
         </div>
+        <div class="quiz-answer"></div>
         `;
+    quizAnswer = document.querySelector('.quiz-answer');
     data[current].answer.forEach(
-        (e, index) => quizContent.innerHTML +=
+        (e, index) => quizAnswer.innerHTML +=
         `<div class="quiz-item">
             <input type="radio" id="answer-${index + 1}" name="question-${current + 1}">
             <label for="answer-${index + 1}">
@@ -74,48 +91,59 @@ function initialize() {
         </div>
         `
     );
-    
-    let quizItem = document.querySelectorAll('.quiz-item');
 
-    quizItem.forEach(
-        (e, index) => {
-            // for (let i = 0; i < dataStore.length; i ++) {
-            //     if (dataStore[i].current == current && dataStore[i].index == index) {
-            //         e.checked = true;
-            //     }
-            // }
-            e.addEventListener('click', () => {
-                let input = e.getElementsByTagName('input')[0];
-                input.checked = true;
-                dataStore.push({current: current, index: index});
-                console.log(dataStore);
-                next.classList.remove('disable');
-            });
-        }
-    );
-    
-    
+    input = document.getElementsByTagName('input');
+
+    storeData();
     checkProgress();
     showSubmit();
 
-
 }
-    
+
+    [...input].forEach(
+        (e, index) => e.addEventListener('click', () => {
+            e.checked = true;
+            dataStore.forEach(item => {
+              if (item.current == current) {
+                item.index = index;
+              }
+            });
+            next.classList.remove('disable');
+            console.log(dataStore);
+        })
+    );
+
     next.addEventListener('click', () => {
         if (current >= data.length - 1) return
         current ++;
+        storeData();
         reset();
         initialize();
+        console.log(current);
+        console.log(dataStore);
     });
 
     pre.addEventListener('click', () => {
         if (current <= 0) return
         current --;
+        storeData();
         reset();
         initialize();
+        console.log(current);
+        console.log(dataStore);
     });
 
-   
+    function storeData() {
+      dataStore.forEach((item, index) => {
+        if (item.current == current) {
+          for (let i = 0; i < input.length; i ++) {
+            if (input[i].checked == true) {
+              item.index = i;
+            }
+          }
+        }
+      });
+    }
 
     function checkProgress() {
         if (current == data.length - 1) {
@@ -132,6 +160,3 @@ function initialize() {
             finish.style.display = 'none';
         }
     }
-
-    
-
